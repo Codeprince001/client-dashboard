5f4989466e294f74e5094cc6a20be7dbb0f8cee2
Codeprince001 <urangyorkwisdom@gmail.com>
2023-07-20 12:43:05 +0100

    created custom BarchartTooltip

--- src/Components/BarChart/BarChartBox.js
+++ src/Components/BarChart/BarChartBox.js
@@ -6,6 +6,21 @@ import "./barchart.css";
 const BarChartBox = (props) => {
   const theme = useTheme();
 
+  const CustomTooltip = ({ active, payload, label }) => {
+    if (active && payload && payload.length) {
+      const data = payload[0].payload; // Extract the data for the tooltip
+
+      return (
+        <div style={{ backgroundColor: theme.palette.custom.darkBlue, display: "flex", flexDirection: "column", borderRadius: "10px", padding: "1rem", fontSize: "12px" }}>
+          <p style={{ color: theme.palette.custom.grey, borderBottom: `2px solid ${theme.palette.custom.blue}` }}>{data.xData}</p>
+          <p style={{ color: theme.palette.custom.white, margin: "5px", textAlign: "center" }}>{data.yData}</p>
+          <p style={{ color: theme.palette.custom.grey, textAlign: "center" }}>Earnings</p>
+        </div >
+      );
+    }
+
+    return null;
+  };
 
   return (
     <div className="barChartBox">
@@ -16,13 +31,14 @@ const BarChartBox = (props) => {
         <ResponsiveContainer width="99%" height="100%">
           <BarChart data={props.chartData} barGap={2} margin={{ top: 20, right: 20, bottom: 20, left: 20 }}>
             <Tooltip
+              content={<CustomTooltip />}
               cursor={{ fill: "none" }}
               labelStyle={{ color: theme.palette.custom.grey }}
               contentStyle={{ background: theme.palette.custom.darkBlue, fontSize: "12px", border: "none", borderRadius: "10px" }}
             />
-            <XAxis axisLine={false} padding={{ right: 10 }} tickMargin={10} tickLine={false} dataKey="xdata" fontSize="12px" />
-            <YAxis axisLine={false} tickMargin={10} fontSize="12px" />
-            <Bar dataKey="uv" fill={theme.palette.custom.blue} />
+            <XAxis axisLine={false} padding={{ right: 10 }} tickMargin={10} tickLine={false} dataKey="xData" fontSize="12px" />
+            <YAxis axisLine={false} tickMargin={10} fontSize="12px" dataKey="yData" />
+            <Bar dataKey="yData" fill={theme.palette.custom.blue} />
           </BarChart>
         </ResponsiveContainer>
       </div>
