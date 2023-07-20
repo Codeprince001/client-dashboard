fa1b1b2070cf2064ebaf49c1ae238d9664938b07
Codeprince001 <urangyorkwisdom@gmail.com>
2023-07-20 14:21:36 +0100

    fix barchar Data

--- src/Components/BarChart/BarChartBox.js
+++ src/Components/BarChart/BarChartBox.js
@@ -11,8 +11,8 @@ const BarChartBox = (props) => {
       const data = payload[0].payload; // Extract the data for the tooltip
 
       return (
-        <div style={{ backgroundColor: theme.palette.custom.darkBlue, display: "flex", flexDirection: "column", borderRadius: "10px", padding: "1rem", fontSize: "12px" }}>
-          <p style={{ color: theme.palette.custom.grey, borderBottom: `2px solid ${theme.palette.custom.blue}` }}>{data.xData}</p>
+        <div style={{ backgroundColor: theme.palette.custom.darkBlue, display: "flex", flexDirection: "column", borderRadius: "10px", padding: ".2rem", fontSize: "12px" }}>
+          <p style={{ margin: "5px", color: theme.palette.custom.grey, borderBottom: `2px solid ${theme.palette.custom.blue}` }}>{`${label}`}</p>
           <p style={{ color: theme.palette.custom.white, margin: "5px", textAlign: "center" }}>{data.yData}</p>
           <p style={{ color: theme.palette.custom.grey, textAlign: "center" }}>Earnings</p>
         </div >
@@ -37,7 +37,7 @@ const BarChartBox = (props) => {
               contentStyle={{ background: theme.palette.custom.darkBlue, fontSize: "12px", border: "none", borderRadius: "10px" }}
             />
             <XAxis axisLine={false} padding={{ right: 10 }} tickMargin={10} tickLine={false} dataKey="xData" fontSize="12px" />
-            <YAxis axisLine={false} tickMargin={10} fontSize="12px" dataKey="yData" />
+            <YAxis tickCount={4} axisLine={false} tickMargin={10} fontSize="12px" dataKey="yData" />
             <Bar dataKey="yData" fill={theme.palette.custom.blue} />
           </BarChart>
         </ResponsiveContainer>
