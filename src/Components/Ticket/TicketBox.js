a8a6f9efb86e6c2d195dc22b848e10593d3b950c
Codeprince001 <urangyorkwisdom@gmail.com>
2023-08-07 12:36:02 +0100

    feature: styled ticket box

--- src/Components/Ticket/TicketBox.js
+++ src/Components/Ticket/TicketBox.js
@@ -10,22 +10,24 @@ const TicketBox = ({ icon }) => {
       {
         TicketHolders.map((data) => {
           return (
-            <Box key={data.id} style={{ margin: "10px" }}>
-              <div style={{ display: "flex", alignItems: "center", marginBottom: "10px" }}>
-                <img src={data.img} alt='user identity' width="50px" height="50px" style={{ borderRadius: "5px", marginRight: "5px" }} />
-                <div style={{ display: "flex" }}>
-                  <div style={{ marginRight: "30px" }}>
-                    <div style={{ fontSize: "12px", fontWeight: 500 }}>{data.description}</div>
-                    <div style={{ fontSize: "8px", fontWeight: "bold", color: theme.palette.custom.grey }}>{data.name}</div>
-                  </div>
-                  <div style={{ display: "flex", gap: "10px", alignItems: "center" }}>
-                    <div style={{ fontSize: "12px", fontWeight: 500 }}>{data.date}</div>
-                    {data.state && <div className='state-color' style={{ width: "10px", height: "10px", borderRadius: "50%", backgroundColor: "red", border: "1px solid red" }}>.</div>}
+            <>
+              <Box key={data.id} style={{ margin: "10px" }}>
+                <div style={{ display: "flex", alignItems: "center", marginBottom: "10px" }}>
+                  <img src={data.img} alt='user identity' width="50px" height="50px" style={{ borderRadius: "5px", marginRight: "5px" }} />
+                  <div style={{ display: "flex" }}>
+                    <div style={{ marginRight: "30px" }}>
+                      <div style={{ fontSize: "12px", fontWeight: 500 }}>{data.description}</div>
+                      <div style={{ fontSize: "8px", fontWeight: "bold", color: theme.palette.custom.grey }}>{data.name}</div>
+                    </div>
+                    <div style={{ display: "flex", gap: "10px", alignItems: "center" }}>
+                      <div style={{ fontSize: "12px", fontWeight: 500 }}>{data.date}</div>
+                      {data.state && <div className='state-color' style={{ width: "10px", height: "10px", borderRadius: "50%", backgroundColor: "red", border: "1px solid red" }}>.</div>}
+                    </div>
                   </div>
                 </div>
-              </div>
+              </Box>
               <div style={{ borderBottom: "1px solid #d9d9d9", width: "100%" }}></div>
-            </Box>
+            </>
           );
         })
       }
