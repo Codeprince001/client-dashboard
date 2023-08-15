9dcc3d2682b1e142ec0bbb5fed9d915e72c25c7a
Codeprince001 <urangyorkwisdom@gmail.com>
2023-08-15 10:24:55 +0100

    fix layout styles

--- src/Components/Ticket/TicketBox.js
+++ src/Components/Ticket/TicketBox.js
@@ -1,37 +1,40 @@
 import { Box, useTheme } from '@mui/material';
 import { TicketHolders } from '../../Data/Data';
+import "./ticket.css";
 
 const TicketBox = ({ icon }) => {
   const theme = useTheme();
 
   return (
-    <div className="box-container" style={{ width: "100%", height: "100%", overflow: "auto" }}>
-      <Box sx={{ padding: "1rem", position: 'sticky', top: '0', zIndex: "1", backgroundColor: theme.palette.custom.white, display: "flex", alignItem: "center" }}><div style={{ fontSize: "1rem", fontWeight: "600", marginRight: "5px" }}>Open Tickets</div><span style={{ fontSize: "1.2rem" }}>{icon}</span></Box>
+    <Box className="box">
+      <Box className="box-container" sx={{ padding: "1rem", position: 'sticky', top: '0', zIndex: "1", backgroundColor: theme.palette.custom.white, display: "flex", alignItem: "center" }}><div style={{ fontSize: "1rem", fontWeight: "600", marginRight: "5px" }}>Open Tickets</div><span style={{ fontSize: "1.2rem" }}>{icon}</span></Box>
       {
         TicketHolders.map((data) => {
           return (
             <>
-              <Box key={data.id} style={{ margin: "10px" }}>
-                <div style={{ display: "flex", alignItems: "center", marginBottom: "10px" }}>
-                  <img src={data.img} alt='user identity' width="50px" height="50px" style={{ borderRadius: "5px", marginRight: "5px" }} />
-                  <div style={{ display: "flex" }}>
+              <Box className="ticket-holder" key={data.id} >
+                <div className="ticket-holder-container">
+                  <div className="ticket-holder-details" >
+                    <div className='ticket-holder-img'>
+                      <img src={data.img} alt='user identity' width="40px" height="40px" style={{ borderRadius: "5px", marginRight: "5px" }} />
+                    </div>
                     <div style={{ marginRight: "30px" }}>
-                      <div style={{ fontSize: "12px", fontWeight: 500 }}>{data.description}</div>
+                      <div style={{ fontSize: "12px", fontWeight: 500, maxWidth: "350px" }}>{data.description}</div>
                       <div style={{ fontSize: "8px", fontWeight: "bold", color: theme.palette.custom.grey }}>{data.name}</div>
                     </div>
-                    <div style={{ display: "flex", gap: "10px", alignItems: "center" }}>
-                      <div style={{ fontSize: "12px", fontWeight: 500 }}>{data.date}</div>
-                      {data.state && <div className='state-color' style={{ width: "10px", height: "10px", borderRadius: "50%", backgroundColor: "red", border: "1px solid red" }}>.</div>}
-                    </div>
+                  </div>
+                  <div className='ticket-state-date' >
+                    <div style={{ fontSize: "12px", fontWeight: 500 }}>{data.date}</div>
+                    {data.state && <div className='state-color'></div>}
                   </div>
                 </div>
               </Box>
-              <div style={{ borderBottom: "1px solid #d9d9d9", width: "100%" }}></div>
+              <div className='bottom-border'></div>
             </>
           );
         })
       }
-    </div>
+    </Box>
   );
 };
 
